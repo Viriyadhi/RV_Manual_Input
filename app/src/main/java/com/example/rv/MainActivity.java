bf3dded93f1f3baeeb3f0b1ec58aeea8eaa5a15a
viriyadhitjan <viriyadhitjan@gmail.com>
2021-08-23 00:27:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/rv/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.rv;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+private ArrayList<User> usersList;
+private  RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycler);
+        usersList = new ArrayList<>();
+
+
+        setUserInfo();
+        setAdapter ();
+    }
+
+    private void setAdapter() {
+        recyclerAdapter adapter = new recyclerAdapter(usersList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void setUserInfo() {
+
+        usersList.add(new User("John"));
+        usersList.add(new User("Bob"));
+        usersList.add(new User("Cindy"));
+    }
+
+
+}
